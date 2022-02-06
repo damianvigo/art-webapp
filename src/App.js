c80e9bd021e146fc83d60267ed45eb0f244111e4
Damian Vigo <damiandanielvigo@gmail.com>
2022-02-06 14:22:04 -0300

    change-helmet

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Home from './pages/Home';
 import MisObras from './pages/MisObras';
 import Contacto from './pages/Contacto';
 import Error404 from './pages/Error404';
-import ico from './assets/icon/favicon.ico';
-import homeImg from './assets/img/homeImg.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -15,7 +13,6 @@ import ScrollToTop from './components/ScrollToTop';
 import { useContext } from 'react';
 import ThemeContext from './context/ThemeContext';
 import BtnDarkMode from './components/BtnDarkMode';
-import { Helmet } from 'react-helmet';
 import { GlobalStyle } from './components/CreateGlobalStyle';
 
 AOS.init({
@@ -28,47 +25,6 @@ function App() {
   const { toTop, scrollTop, setScrollTop } = useScrollTop();
   return (
     <>
-      <Helmet>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="Hola bienvenid@ a mi portafolio artístico Soy Adriana Stigliano, Artista Plástica"
-        />
-        <link rel="canonical" href="https://adrianastiglianoarte.netlify.app" />
-        <link rel="icon" type="image/x-icon" href={ico} />
-        <link rel="apple-touch-icon" href={ico} />
-        <meta name="theme-color" content="#e0aaff" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          name="twitter:title"
-          content="Portafolio Artístico Adriana Stigliano"
-        />
-        <meta
-          property="og:image"
-          name="twitter:image"
-          content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
-        />
-        <meta
-          property="og:url"
-          name="twitter:url"
-          content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
-        />
-        <meta
-          property="og:description"
-          name="twitter:description"
-          content="Portafolio Artístico Adriana Stigliano"
-        />
-      </Helmet>
       <GlobalStyle />
       <HashRouter>
         <ScrollToTop />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import ico from '../assets/icon/favicon.ico';
+import homeImg from '../assets/img/homeImg.jpg';
 import Banner from '../components/Banner';
 import Conoceme from '../components/Conoceme';
 import Footer from '../components/Footer';
@@ -17,7 +19,45 @@ const Home = ({ theme }) => {
   return (
     <>
       <Helmet>
-        <title>Bienvenid@</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          name="description"
+          content="Hola bienvenid@ a mi portafolio artístico Soy Adriana Stigliano, Artista Plástica"
+        />
+        <link rel="canonical" href="https://adrianastiglianoarte.netlify.app" />
+        <link rel="icon" type="image/x-icon" href={ico} />
+        <link rel="apple-touch-icon" href={ico} />
+        <meta name="theme-color" content="#e0aaff" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          name="twitter:title"
+          content="Portafolio Artístico Adriana Stigliano"
+        />
+        <meta
+          property="og:image"
+          name="twitter:image"
+          content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
+        />
+        <meta
+          property="og:url"
+          name="twitter:url"
+          content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
+        />
+        <meta
+          property="og:description"
+          name="twitter:description"
+          content="Portafolio Artístico Adriana Stigliano"
+        />
       </Helmet>
       <Header />
       <main>

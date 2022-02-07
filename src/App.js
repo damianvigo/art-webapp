4084c4e19554b10792a06552a01678357d91aa0e
Damian Vigo <damiandanielvigo@gmail.com>
2022-02-07 10:20:36 -0300

    change-cards

--- src/App.js
+++ src/App.js
@@ -29,15 +29,65 @@ function App() {
     <>
       <GlobalStyle />
       <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://adrianastiglianoarte.netlify.app" />
+        <meta name="theme-color" content="#e0aaff" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css"
+          integrity="sha512-+mlclc5Q/eHs49oIOCxnnENudJWuNqX5AogCiqRBgKnpoplPzETg2fkgBFVC6WYUVxYYljuxPNG8RE7yBy1K+g=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" type="image/x-icon" href={ico} />
         <link rel="apple-touch-icon" href={ico} />
-        <title>Inicio</title>
+        <title>Portafolio Artístico | Adriana Stigliano</title>
+        <meta name="title" content="Portafolio Artístico | Adriana Stigliano" />
         <meta
-          property="og:image"
+          name="description"
+          content="Hola bienvenid@ a mi portafolio artístico Soy Adriana Stigliano, Artista Plástica"
+        />
+
+        <meta name="og:type" content="website" />
+        <meta
+          name="og:url"
+          content="https://adrianastiglianoarte.netlify.app"
+        />
+        <meta
+          name="og:title"
+          content="Portafolio Artístico | Adriana Stigliano"
+        />
+        <meta
+          name="og:description"
+          content="Hola bienvenid@ a mi portafolio artístico Soy Adriana Stigliano, Artista Plástica"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:url"
+          content="https://adrianastiglianoarte.netlify.app"
+        />
+        <meta
+          name="twitter:title"
+          content="Portafolio Artístico | Adriana Stigliano"
+        />
+        <meta
+          name="twitter:description"
+          content="Hola bienvenid@ a mi portafolio artístico Soy Adriana Stigliano, Artista Plástica"
+        />
+        <meta
+          name="og:image"
           content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
         />
         <meta
-          property="twitter:image"
+          name="twitter:image"
           content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
         />
       </Helmet>

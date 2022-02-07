bd519c6c28b4ca3562957a1fc60804d0e06060d4
Damian Vigo <damiandanielvigo@gmail.com>
2022-02-07 12:00:33 -0300

    into-hashrouter

--- src/App.js
+++ src/App.js
@@ -30,49 +30,50 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Helmet>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="canonical" href="https://adrianastiglianoarte.netlify.app" />
-        <meta name="theme-color" content="#e0aaff" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" type="image/x-icon" href={ico} />
-        <link rel="apple-touch-icon" href={ico} />
-        <title>Portafolio Artístico | Adriana Stigliano</title>
-        <meta name="title" content="Portafolio Artístico | Adriana Stigliano" />
-        <meta
-          name="description"
-          content="Hola bienvenid@ a mi Portafolio artístico"
-        />
-        <meta name="twitter:card" content="summary" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          name="twitter:title"
-          content="Portafolio Artístico | Adriana Stigliano"
-        />
-        <meta
-          property="og:description"
-          name="twitter:description"
-          content="Hola bienvenid@ a mi Portafolio artístico"
-        />
-        <meta
-          property="og:image"
-          name="twitter:image"
-          content={`https://adrianastiglianoarte.netlify.app${window.location.hash}${homeImg}`}
-        />
-        <meta
-          property="og:url"
-          name="twitter:url"
-          content={`https://adrianastiglianoarte.netlify.app/${window.location.hash}/`}
-        />
-      </Helmet>
       <HashRouter>
+        <Helmet>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="canonical"
+            href="https://adrianastiglianoarte.netlify.app"
+          />
+          <meta name="theme-color" content="#e0aaff" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" type="image/x-icon" href={ico} />
+          <link rel="apple-touch-icon" href={ico} />
+          <title>Portafolio Artístico | Adriana Stigliano</title>
+          <meta
+            name="title"
+            content="Portafolio Artístico | Adriana Stigliano"
+          />
+          <meta
+            name="description"
+            content="Hola bienvenid@ a mi Portafolio artístico"
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            name="twitter:title"
+            content="Portafolio Artístico | Adriana Stigliano"
+          />
+          <meta
+            property="og:image"
+            name="twitter:image"
+            content={`https://adrianastiglianoarte.netlify.app${homeImg}`}
+          />
+          <meta
+            property="og:url"
+            name="twitter:url"
+            content={`https://adrianastiglianoarte.netlify.app`}
+          />
+        </Helmet>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home theme={theme} />} />
